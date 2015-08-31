21ac60d25d2f6a13251cd6e01c2a86e323cd0b9e
newsn31 <quang.vo31@gmail.com>
2015-08-30 22:05:50 -0700

    trying grunt

---
+++ gulp.js
@@ -0,0 +1,54 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+});
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Optimize PNGs
+gulp.task('png', function () {
+    return gulp.src('images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+// Optimize Jpgs
+gulp.task('jpgs', function() {
+    return gulp.src('images/*.jpg')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('dist/images'));
+});
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
